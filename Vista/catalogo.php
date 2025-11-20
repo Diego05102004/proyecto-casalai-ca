c9e8fbc5cbe3e31cd0114c1105a9be2e57293aeb
SaymonXP2114 <scorpiogaming21103@gmail.com>
2025-11-20 07:58:11 -0400

    Update catalogo.php

--- Vista/catalogo.php
+++ Vista/catalogo.php
@@ -352,22 +352,23 @@
             </div>
         </div>
 
-    <!-- Modal para confirmar cambio de estado -->
-    <div class="modal fade" id="cambioEstadoModal" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Cambiar Estado del Combo</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>¿Está seguro que desea <span id="accionEstado">habilitar</span> este combo?</p>
-                    <input type="hidden" id="combo_id_estado" value="">
-                    <input type="hidden" id="nuevo_estado" value="1">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary" id="confirmarCambioEstado">Confirmar</button>
+        <!-- Modal para confirmar cambio de estado -->
+        <div class="modal fade" id="cambioEstadoModal" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Cambiar Estado del Combo</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>¿Está seguro que desea <span id="accionEstado">habilitar</span> este combo?</p>
+                        <input type="hidden" id="combo_id_estado" value="">
+                        <input type="hidden" id="nuevo_estado" value="1">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-primary" id="confirmarCambioEstado">Confirmar</button>
+                    </div>
                 </div>
             </div>
         </div>
