dcc8ebd6e91d8c7b7a9e0501e0ab4b04b6efc747
SaymonXP2114 <scorpiogaming21103@gmail.com>
2026-02-13 22:22:45 -0400

    ...

--- Vista/proveedor.php
+++ Vista/proveedor.php
@@ -1262,8 +1262,14 @@ aria-labelledby="modificarProveedorModalLabel" aria-hidden="true">
         title="Visualizar Reportes"
         onclick="window.location.href='?pagina=reporteProveedores'">
         <img src="assets/img/grafic.png" alt="Reportes" width="30" height="30">
-    
     </button>
+    <button 
+        class="btn-ayuda"
+        title="Visualizar Ayuda"
+        onclick="window.location.href='?pagina=ayuda'">
+        <img src="assets/img/info-ayuda.svg" alt="Ayuda" width="20" height="20">
+    </button>
+
 </body>
 </html>
 <?php
