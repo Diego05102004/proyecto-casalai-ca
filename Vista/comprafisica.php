466d2a2d0e05d1c7e2ebe0fe76613fd35f9d1ab9
SaymonXP2114 <scorpiogaming21103@gmail.com>
2026-02-19 21:13:57 -0400

    ayuda venta

--- Vista/comprafisica.php
+++ Vista/comprafisica.php
@@ -96,6 +96,9 @@ aria-labelledby="registrarCompraFisicaModalLabel" aria-hidden="true">
         <div class="modal-content">
             <form id="f" method="POST" enctype="multipart/form-data" onsubmit="return validarFormularioCompra()" novalidate>
                 <div class="modal-header">
+                    <button type="button" class="btn-ayuda-modal" title="Ayuda para Registrar Venta Presencial" data-contexto="registrar">
+                        <img src="assets/img/info-ayuda.svg" alt="Ayuda" width="18" height="18">
+                    </button>
                     <h5 class="titulo-form" id="registrarCompraFisicaModalLabel">Incluir Venta Presencial</h5>
                     <button type="button" class="close" data-bs-dismiss="modal" aria-label="Cerrar">
                         <span aria-hidden="true">&times;</span>
@@ -553,8 +556,7 @@ $(document).on('click', '.btn-eliminar-producto', function () {
 </script>
     <button 
         class="btn-ayuda"
-        title="Visualizar Ayuda"
-        onclick="window.location.href='?pagina=ayuda'">
+        title="Visualizar Ayuda">
         <img src="assets/img/info-ayuda.svg" alt="Ayuda" width="20" height="20">
     </button>
 </body>
