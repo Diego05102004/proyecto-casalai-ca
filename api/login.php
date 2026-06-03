e795ae89467cb558cc0fe112ba61ff745089861f
Darckort <darckortgame@gmail.com>
2026-06-03 08:26:43 -0400

    UPDATE API

--- api/login.php
+++ api/login.php
@@ -17,8 +17,7 @@
 
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/auth.php';
-
-use Usuario\ProyectoCasalaiCa\Modelo\Clases\Login;
+require_once __DIR__ . '/../Modelo/Usuario/ProyectoCasalaiCa/Clases/Login.php';
 
 // Solo permitir método POST
 validateMethod(['POST']);
--- api/recuperar.php
+++ api/recuperar.php
@@ -10,8 +10,7 @@
  */
 
 require_once __DIR__ . '/config.php';
-
-use Usuario\ProyectoCasalaiCa\Modelo\Clases\Login;
+require_once __DIR__ . '/../Modelo/Usuario/ProyectoCasalaiCa/Clases/Login.php';
 
 // Solo permitir método POST
 validateMethod(['POST']);
